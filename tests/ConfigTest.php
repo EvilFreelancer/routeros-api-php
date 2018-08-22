b2f5f7a44c5a0e4759d6b0172d7ce756b3d019ff
Paul Rock <paul@drteam.rocks>
2018-08-23 01:02:58 +0300

    code coverage via sc enabled

---
+++ tests/ConfigTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pasha
+ * Date: 23.08.18
+ * Time: 0:43
+ */
+
+namespace RouterOS\Tests;
+
+use RouterOS\Config;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetParameters()
+    {
+
+    }
+
+    public function testSet()
+    {
+
+    }
+
+    public function testGet()
+    {
+
+    }
+}
