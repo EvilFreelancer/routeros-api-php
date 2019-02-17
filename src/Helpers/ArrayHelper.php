fa615d670a11ae98d5681302aa437008b927a6b7
Paul Rock <paul@drteam.rocks>
2019-02-17 13:29:55 +0300

    ArrayHelper update, unit tests added

--- src/Helpers/ArrayHelper.php
+++ src/Helpers/ArrayHelper.php
@@ -11,25 +11,7 @@ namespace RouterOS\Helpers;
 class ArrayHelper
 {
     /**
-     * Check if required keys in array
-     *
-     * @param   array $keys
-     * @param   array $array
-     * @return  string|bool Return true if all fine, and string with name of key which was not found
-     */
-    public static function checkIfKeysNotExist(array $keys, array $array)
-    {
-        $output = [];
-        foreach ($keys as $key) {
-            if (!array_key_exists($key, $array) && isset($array[$key])) {
-                $output[] = $key;
-            }
-        }
-        return !empty($output) ? implode(',', $output) : true;
-    }
-
-    /**
-     * Check if key in list of parameters
+     * Check if required single key in array of parameters
      *
      * @param   string $key
      * @param   array  $array
@@ -37,6 +19,24 @@ class ArrayHelper
      */
     public static function checkIfKeyNotExist(string $key, array $array): bool
     {
-        return (!array_key_exists($key, $array));
+        return (!array_key_exists($key, $array) && empty($array[$key]));
+    }
+
+    /**
+     * Check if required keys in array of parameters
+     *
+     * @param   array $keys
+     * @param   array $array
+     * @return  array|bool Return true if all fine, and string with name of key which was not found
+     */
+    public static function checkIfKeysNotExist(array $keys, array $array)
+    {
+        $output = [];
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $array) && empty($array[$key])) {
+                $output[] = $key;
+            }
+        }
+        return !empty($output) ? $output : true;
     }
 }
---
+++ tests/Helpers/ArrayHelperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RouterOS\Helpers;
+
+use PHPUnit\Framework\TestCase;
+
+class ArrayHelperTest extends TestCase
+{
+    public function testCheckIfKeyNotExist()
+    {
+        $test1 = ArrayHelper::checkIfKeyNotExist(1, [0 => 'a', 1 => 'b', 2 => 'c']);
+        $this->assertFalse($test1);
+
+        $test2 = ArrayHelper::checkIfKeyNotExist('a', [1 => 'a', 2 => 'b', 3 => 'c']);
+        $this->assertTrue($test2);
+    }
+
+    public function testCheckIfKeysNotExist()
+    {
+        $test1 = ArrayHelper::checkIfKeysNotExist([1, 2], [0 => 'a', 1 => 'b', 2 => 'c']);
+        $this->assertTrue($test1);
+
+        $test2 = ArrayHelper::checkIfKeysNotExist([3, 4], [0 => 'a', 1 => 'b', 2 => 'c']);
+        $this->assertNotTrue($test2);
+    }
+}
