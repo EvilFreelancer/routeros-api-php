407c8a9fd3216f24dbec38b22950c5310ba2ad34
Paul Rock <paul@drteam.rocks>
2019-03-24 16:17:01 +0300

    typo if tests fixed

--- tests/Helpers/BinaryStringHelperTest.php
+++ tests/Helpers/BinaryStringHelperTest.php
@@ -34,10 +34,6 @@ class BinaryStringHelperTest extends TestCase
             // 
             // [0xFFFFFFFFFFFFFFFF, chr(0xFF).chr(0xFF).chr(0xFF).chr(0xFF).chr(0xFF).chr(0xFF).chr(0xFF).chr(0xFF)],
 
-            // -1 is encoded with 0xFFFFFFF..... 
-            // 64 bits maximal value (on a 64 bits system only)
-            [-1, chr(0xFF) . chr(0xFF) . chr(0xFF) . chr(0xFF) . chr(0xFF) . chr(0xFF) . chr(0xFF) . chr(0xFF)], // 64 bits upper boundary value
-
             // Let's try random value
             [0x390DDD99, chr(0x39) . chr(0x0D) . chr(0xDD) . chr(0x99)],
         ];
