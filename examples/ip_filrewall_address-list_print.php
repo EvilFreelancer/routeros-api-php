b826afd9a62a16d6ee3b5d4afbf2dd5bfb58cbe9
Paul Rock <paul@drteam.rocks>
2019-07-20 15:26:40 +0300

    yet another example added

--- examples/ip_filrewall_address-list_print.php
+++ examples/ip_filrewall_address-list_print.php
@@ -3,16 +3,42 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
 
-use \RouterOS\Rosario;
+use \RouterOS\Client;
 
 // Initiate client with config object
-$client = new Rosario([
+$client = new Client([
     'timeout' => 1,
     'host'    => '127.0.0.1',
     'user'    => 'admin',
     'pass'    => 'admin'
 ]);
 
-// Send query to RouterOS
+// Send query to RouterOS and parse response
 $response = $client->write('/ip/firewall/address-list/print')->read();
-print_r($response);
+
+// You could treat response as an array except using array_* function
+
+// Export every row using foreach
+foreach ($response as $row) {
+    echo current($row) . PHP_EOL;
+}
+
+$item = current($response);
+var_dump($item);
+echo PHP_EOL;
+
+$item = end($response);
+var_dump($item);
+echo PHP_EOL;
+
+$item = current($response);
+var_dump($item);
+echo PHP_EOL;
+
+$item = reset($response);
+var_dump($item);
+echo PHP_EOL;
+
+$item = current($response);
+var_dump($item);
+echo PHP_EOL;
